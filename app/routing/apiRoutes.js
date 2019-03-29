da0547ad7c90c3dace3f76d373a6c6b7f91b65f4
Edward Hopper <burson.kate@gmail.com>
2019-03-28 18:39:32 -0700

    post error

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,31 @@
-var friendsData = require('../data/friends');
+var friends = require('../data/friends');
+
 
 module.exports = function(app){
-
     app.get('/api/friends', function(req, res){
-        res.send('test get');
-        res.end();
+        res.json(friendsData);
     });
 
     app.post('/api/friends', function(req,res){
-        res.send('/api/friends post');
-        res.end();
+        console.log('posted!');
+        newFriend = friends.friends[indexOf(friends.length -1)];
+        function compare(newFriend){
+            for(var i = 0; i < friends.length; i++){
+                var bestFriendValue = 100;
+                var bestFriend = {};
+                for(var j = 0; j < friend.scores.length; i++){
+                    var diff = Math.abs(newFriend.scores[j] - friend[i].scores[j]);
+                    difference += diff;
+                    if(difference < bestFriendValue){
+                        bestFriendValue = difference;
+                        bestFriend = friend[i];
+                    } else {
+                        return
+                    }
+                }
+            }
+            
+        }
+        compare(newFriend);
     });
-
 }
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+// var path = require('path');
+var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 3000;
 
@@ -8,6 +10,7 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+// app.use(bodyParser.text());
 
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
