beb91993ede181113ce4bafcede2f08e0417b94a
Edward Hopper <burson.kate@gmail.com>
2019-03-29 23:14:48 -0700

    survey.html functional apiRoutes not posting

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,31 +1,37 @@
 var friends = require('../data/friends');
 
-
 module.exports = function(app){
+
     app.get('/api/friends', function(req, res){
-        res.json(friendsData);
+        res.json(friends);
     });
 
     app.post('/api/friends', function(req,res){
-        console.log('posted!');
-        newFriend = friends.friends[indexOf(friends.length -1)];
-        function compare(newFriend){
-            for(var i = 0; i < friends.length; i++){
-                var bestFriendValue = 100;
-                var bestFriend = {};
-                for(var j = 0; j < friend.scores.length; i++){
-                    var diff = Math.abs(newFriend.scores[j] - friend[i].scores[j]);
-                    difference += diff;
-                    if(difference < bestFriendValue){
-                        bestFriendValue = difference;
-                        bestFriend = friend[i];
-                    } else {
-                        return
-                    }
-                }
+        newFriend = req.body;
+        friends.push(newFriend);
+        console.log('new friend', newFriend);
+
+        var difference = 40;
+        var match = {};
+        for(var i = 0; i < friends.length; i++){
+            var newFriendSum =  newFriend.scores.reduce((a,b) => a + b, 0);
+            var friendSum =  friends[i].scores.reduce((a,b) => a + b, 0);
+            var diff = Math.abs(newFriendSum - friendSum);
+            if(diff < difference){
+                difference = diff;
+                match.name = friends[i].name;
+                match.photo = friends[i].photo;
+                console.log(match);
+                return match;
+            } else {
+                return
             }
-            
         }
-        compare(newFriend);
+
+        res.json({
+            name: match.name,
+            photo: match.photo
+        });
     });
 }
+
